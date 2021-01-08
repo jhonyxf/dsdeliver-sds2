350ece4817e07e072fce205b133fb576ad8d422e
sidney_silva_verity <jhonyxf@gmail.com>
2021-01-08 17:15:08 -0300

    Add Location and Mapbox

--- front-web/src/Orders/types.tsx
+++ front-web/src/Orders/types.tsx
@@ -1,7 +1,15 @@
+import OrderLocation from "./OrderLocation"
+
 export type Product = {
     id:number;
     name: string;
     price:number;
     description:string;
     imageUri:string;
+}
+
+export type OrderLocationdata = {
+        latitude: number;
+        longitude: number;
+        address: string;
 }

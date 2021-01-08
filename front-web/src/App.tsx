0f251dcd22c7efb90335e02651323b3eabb80e3f
sidney_silva_verity <jhonyxf@gmail.com>
2021-01-07 22:02:44 -0300

    Add Footer

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
-import Navbar from './navbar';
-import Home from './home';
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
   return (

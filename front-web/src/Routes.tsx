cba7ee8e58d1e4e915b4a858b1f396e0209d77d1
sidney_silva_verity <jhonyxf@gmail.com>
2021-01-09 00:32:12 -0300

    Correção Home

--- front-web/src/Routes.tsx
+++ front-web/src/Routes.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import Home from "./Home";
+import Home from "./home";
 import Navbar from "./Navbar";
 import Orders from "./Orders";
 
